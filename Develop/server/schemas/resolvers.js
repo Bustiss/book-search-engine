8a5fc1fa5b67e99e9e925031d743dfa3f0851212
Willie Jones <williejones432@yahoo.com>
2024-02-26 06:24:49 -0600

    data in graphql updates properly

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -1,24 +1,45 @@
-// import the User model and the signToken function from the auth.js file
-const { User } = require ('../models');
+const { User, Book } = require('../models');
+
+const { AuthenticationError } = require('apollo-server-express');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
+
   Query: {
-    // get a single user by either their id or their username
+    // get single user by either their id or their username
     me: async (parent, args, context) => {
       if (context.user) {
-        const user = await User.findById(context.user._id).populate('savedBooks');
+        const userData = await User.findOne({ _id: context.user._id })
+          .select('-__v -password')
+          .populate('savedBooks');
 
-        return user;
-      } 
+        return userData;
+      }
 
-        throw new AuthenticationError('Not logged in');
-
-    }
+      throw new AuthenticationError('Not logged in');
+    },
   },
 
-  Mutation: { 
-    // create a new user
+  Mutation: {
+    // login a user with a username and password
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    // add a user to the database
     addUser: async (parent, args) => {
       const user = await User.create(args);
       const token = signToken(user);
@@ -26,53 +47,36 @@ const resolvers = {
       return { token, user };
     },
 
-    // create a user, sign a token, and send it back (to client/src/components/SignUpForm.js)
-    login: async (parent, { email, password }) => {
-      const user = await User.findOne({ email });
+    // remove a book from `savedBooks`
+    removeBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        const updatedBooks = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId } } },
+          { new: true }
+        );
 
-      if(!user) {
-        throw new AuthenticationError('Incorrect credentials');
+        return updatedBooks;
       }
-
-      const correctPw = await user.isCorrectPassword(password);
-
-      if (!correctPw) {
-        throw AuthenticationError;
-      }
-
-      const token = signToken(user);
-
-      return { token, user };
-    }
-  },
-
-  // save a book to a user's `savedBooks` field by adding it to the set (to prevent duplicates)
-  saveBook: async (parent, { bookData }, context ) => {
-    if (context.user) {
-      const updatedUser = await User.findByIdAndUpdate(
-        { _id: context.user._id },
-        { $addToSet: { savedBooks: bookData }},
-        { new: true }
-      );
-
-      return updatedUser;
-    }
-
-      throw new Error('Authentication error');
-  },
-
-  // remove a book from `savedBooks`
-  removeBook: async (parent, { bookId }, context) => {
-    if (context.user) {
-      const updatedUser = await User.findByIdAndUpdate(
-        { _id: context.user._id },
-        { $pull: { savedBooks: { bookId }}},
-        { new: true }
-      );
-
-      return updatedUser;
-    }
 
       throw new AuthenticationError('You need to be logged in!');
-  }
-}
+    },
+
+    // add a book to `savedBooks`
+    saveBook: async (parent, { bookToSave }, context) => {
+      if (context.user) {
+        const updatedBooks = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { savedBooks: bookToSave } },
+          { new: true }
+        ).populate('savedBooks');
+
+        return updatedBooks;
+      }
+
+      throw new AuthenticationError('You need to be logged in!');
+    },
+  },
+};
+
+module.exports = resolvers;
--- Develop/server/schemas/typeDefs.js
+++ Develop/server/schemas/typeDefs.js
@@ -1,15 +1,34 @@
-const typeDefs = `
+const { gql } = require('apollo-server-express');
 
-type Query {
-  me: User
-}
+const typeDefs = gql`
+  type Query {
+    me: User
+  }
 
-type Mutation {
-  login(email: String!, password: String!): Auth
-  addUser(username: String!, email: String!, password: String!): Auth
-  saveBook(input: BookInput!): User
-  removeBook(bookId: String!): User
-}
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookToSave: bookInput): User
+    removeBook(bookId: String!): User
+  }
+
+  type Book {
+    bookId: String
+    authors: [String]
+    title: String
+    description: String
+    image: String
+    link: String
+  }
+
+  input bookInput {
+    authors: [String]
+    description: String
+    title: String
+    bookId: String
+    image: String
+    link: String
+  }
 
   type User {
     _id: ID
@@ -18,27 +37,11 @@ type Mutation {
     bookCount: Int
     savedBooks: [Book]
   }
-  
-  type Book {
-    bookId: String
-    authors: [String]
-    description: String
-    title: String
-    image: String
-    link: String
-  }
 
-  input BookInput {
-    description: String
-    title: String
-    image: String
-    link: String
-  }
-  
   type Auth {
     token: ID!
     user: User
   }
-  `
+`;
 
-  module.exports = typeDefs;
+module.exports = typeDefs;
