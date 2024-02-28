bfb74f4bb4c7359282cd482933c4816a70975bc5
Willie Jones <williejones432@yahoo.com>
2024-02-27 19:17:24 -0600

    updated paths to build

--- server/server.js
+++ server/server.js
@@ -31,11 +31,11 @@ const startApolloServer = async (typeDefs, resolvers) => {
 
   // Serve up static assets
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, '../client/dist')));
   }
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
   });
 
   // Connect to the database and then start the server
